2d54783bdaa4d6f0b68a24b8140cd84e73fd4647
ADCAP_SPM <adcapsaopaulo@gmail.com>
2026-04-13 15:03:37 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='adcap-cache-v1';
-const urlsToCache=['/','/index.html'];
-self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});
-self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
